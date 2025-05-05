771f66780a2d586da5b6d6252af62d28914be1e8
rob thijssen <rthijssen@gmail.com>
2025-05-05 16:31:01 +0300

    redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import { Navigate, Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import Header from './components/Header';
-import Dashboard from './components/Dashboard';
 import Chain from './components/Chain';
 import Chains from './components/Chains';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/chains" element={<Header />}>
+    <Route path="/" element={<Header />}>
+      <Route path="/" element={<Navigate to="/chains" />} />
       <Route path="/chains" element={<Chains />} />
       <Route path="/chains/:chain" element={<Chain />} />
     </Route>
