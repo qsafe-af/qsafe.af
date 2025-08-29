e0bc935457c6283fe72c6637bc9e6d7528e0ca16
rob thijssen <rthijssen@gmail.com>
2025-08-29 11:40:07 +0300

    chore: title update

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -311,7 +311,7 @@ const Projects: React.FC = () => {
     return (
       <div className="container">
         <div className="d-flex justify-content-between align-items-center mb-3">
-          <h3>quantum safe project radar</h3>
+          <h3>Quantum-Safe Blockchain Project Radar</h3>
           <div className="btn-group btn-group-sm" role="group">
             <button
               type="button"
